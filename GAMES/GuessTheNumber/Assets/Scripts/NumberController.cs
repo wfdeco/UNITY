f0cb166a0fc67042b2fee6c060693b9e81ef3f88
Deco Filho <wfdeco@gmail.com>
2016-04-03 14:36:51 -0300

    Update Guess the Number
    Fixed the win condition
    Fixed number generator

--- GAMES/GuessTheNumber/Assets/Scripts/NumberController.cs
+++ GAMES/GuessTheNumber/Assets/Scripts/NumberController.cs
@@ -14,18 +14,43 @@ public class NumberController : MonoBehaviour {
 
 	void Start() {
 
+		// SET ALL GUESS NUMBER BOXES TO EMPTY
 		isFull0 = isFull1 = isFull2 = isFull3 = false;
 
+		KeyGenerator();
+
+		// PRINT THE KEY NUMBERS IN CONSOLE
 		for (int i = 0; i < 4; i++) {
-			key [i] = Random.Range (0, 10);
-			print (key [i]);
+			Debug.Log(key[i]);
+		}
+	}
+
+	private void KeyGenerator() {
+
+		// GENERATES 4 DIFFERENT NUMBERS TO THE KEY ARRAY
+		for (int i = 0; i < 4; i++) {
+			if (i == 0) {
+				key[i] = Random.Range(0, 10);
+			} else if (i == 1) {
+				do {
+					key[i] = Random.Range(0, 10);
+				} while (key[i] == key[i-1]);	
+			} else if (i == 2) {
+				do {
+					key[i] = Random.Range(0, 10);
+				} while (key[i] == key[i-1] || key[i] == key[i-2]);
+			} else if (i == 3) {
+				do {
+					key[i] = Random.Range(0, 10);
+				} while (key[i] == key[i-1] || key[i] == key[i-2] || key[i] == key[i-3]);
+			}
 		}
 	}
 
 	public void NumberPressed(string numstr) {
 
-		num = int.Parse (numstr);
-		Debug.Log ("Number pressed: " + num);
+		// COPY THE PRESSED NUMBERS TO GUESS ARRAY
+		num = int.Parse(numstr);
 		GuessCode();
 	}
 		
@@ -54,11 +79,6 @@ public class NumberController : MonoBehaviour {
 				}
 			}
 		}
-
-		print (guess[0]);
-		print (guess[1]);
-		print (guess[2]);
-		print (guess[3]);
 	}
 
 	public void ChangeNum(string z, int x) {
@@ -97,30 +117,9 @@ public class NumberController : MonoBehaviour {
 
 	public void UnlockTry() {
 
-		int gameover = 0;
-
 		LastTry();
-
-		for (int i = 0; i < 4; i++) {
-			if (key [i] == guess [i])
-				gameover++;
-		}
-
-		if (gameover == 4) {
-			Debug.Log ("CONGRATULATIONS! You won!");
-			return;
-		}
-
-		for (int i = 0; i < 4; i++) {
-			for (int j = 0; j < 4; j++) {
-				if (key[i] == guess[j]) {
-					Debug.Log ("Same number found");
-					if (i == j) {
-						Debug.Log ("And in the same place");
-					}
-				}
-			}
-		}
+		DidIWon();
+		ChosenOnes();
 
 		isFull0 = isFull1 = isFull2 = isFull3 = false;
 	}
@@ -134,5 +133,54 @@ public class NumberController : MonoBehaviour {
 			lastTry = lastTry + guess[i].ToString();
 		}
 		cryptoNum.text = lastTry;
+	}
+
+	private void DidIWon() {
+
+		// FIRST WIN CONDITION
+		int gameOver = 0;
+		for (int i = 0; i < 4; i++)
+			if (key[i] == guess[i])
+				gameOver++;
+		if (gameOver == 4) {
+			Debug.Log ("CONGRATULATIONS! You won!");
+
+			// CALL A METHOD FROM ANOTHER SCRIPT
+			LevelManager callWin = GameObject.Find("LevelManager").AddComponent<LevelManager>();
+			callWin.LoadLevel("Win");
+
+			return;
+		}
+	}
+
+	private void ChosenOnes() {
+
+		/* ANALIZAR AS WIN CONDITIONS AQUI */
+
+		int samePlace = 0;
+		int sameNumber = 0;
+
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				//if (guess[j] == -1)
+				//	continue;
+				if (guess[i] == key[j]) {
+					if (i == j)
+						samePlace++;
+					else
+						sameNumber++;
+				}
+			}
+		}
+
+		for (int i = 0; i < 4-1; i++) {
+			for (int j = ++i; j < 4; j++) {
+				if (guess[i] == guess[j])
+					sameNumber--;
+			}
+		}
+
+		Debug.Log("Numbers in the same place: " + samePlace);
+		Debug.Log("Numbers in the wrong place: " + sameNumber);
 	}
 }
